b15feee16e5f19bcbb9e745e98dc25e7016df21a
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-03-02 13:52:29 -0600

    Generate Widget components

---
+++ apps/dashboard/src/app/widgets/widget-details/widget-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fem-production-angular-widget-details',
+  templateUrl: './widget-details.component.html',
+  styleUrls: ['./widget-details.component.scss']
+})
+export class WidgetDetailsComponent  {
+}
---
+++ apps/dashboard/src/app/widgets/widgets.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fem-production-angular-widgets',
+  templateUrl: './widgets.component.html',
+  styleUrls: ['./widgets.component.scss']
+})
+export class WidgetsComponent  {
+}
