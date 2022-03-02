7074bc978f60e86a49dd10f3936ee7723519def5
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-03-02 13:38:37 -0600

    Generate core-data, core-state, and material libs

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -9,6 +9,18 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import {
+  CoreDataModule,
+  coreDataRoutes,
+} from '@fem-production-angular/core-data';
+import {
+  CoreStateModule,
+  coreStateRoutes,
+} from '@fem-production-angular/core-state';
+import {
+  MaterialModule,
+  materialRoutes,
+} from '@fem-production-angular/material';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -28,6 +40,9 @@ import { environment } from '../environments/environment';
     ),
     EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
