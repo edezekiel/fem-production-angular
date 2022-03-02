5bdc00e4de4dc78e8911ecb98c1f79f0fd8188f0
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-03-02 14:07:50 -0600

    Organize app.module imports and ordering

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,37 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
+import { CoreDataModule } from '@fem-production-angular/core-data';
+import { CoreStateModule } from '@fem-production-angular/core-state';
+import { MaterialModule } from '@fem-production-angular/material';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import {
-  CoreDataModule,
-  coreDataRoutes,
-} from '@fem-production-angular/core-data';
-import {
-  CoreStateModule,
-  coreStateRoutes,
-} from '@fem-production-angular/core-state';
-import { MaterialModule } from '@fem-production-angular/material';
-import { RoutingModule } from './routing.module';
-import { WidgetsComponent } from './widgets/widgets.component';
-import { WidgetListComponent } from './widgets/widget-list/widget-list.component';
-import { WidgetDetailsComponent } from './widgets/widget-details/widget-details.component';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RoutingModule } from './routing.module';
+import { WidgetDetailsComponent } from './widgets/widget-details/widget-details.component';
+import { WidgetListComponent } from './widgets/widget-list/widget-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NxWelcomeComponent,
-    WidgetsComponent,
-    WidgetListComponent,
-    WidgetDetailsComponent,
     HomeComponent,
+    NxWelcomeComponent,
+    WidgetDetailsComponent,
+    WidgetListComponent,
+    WidgetsComponent,
   ],
   imports: [
     BrowserModule,
